44ec6184a536ed8e413259a45e751bf428e3c174
Abinash Mohanty <mohantyabinash35@gmail.com>
2025-09-18 19:13:36 +0530

    update MongoDB connection string

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const mongoose=require("mongoose");
 const mongodb=()=>{
-    mongoose.connect(process.env.MONGO_URL)
+    mongoose.connect(process.env.MONGO_URL,{
+        useNewUrlParser: true,
+         useUnifiedTopology: true,
+    })
     .then(()=>{
         console.log("mongodb is connected ");
     })
